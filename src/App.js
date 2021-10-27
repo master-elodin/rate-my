0eb72d1e3764986505d291041e6f537088a972e0
Tim VanDoren <tvandoren@cribl.io>
2021-10-26 21:35:42 -0500

    add search box to header

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import Home from "./Home";
 import Submit from "./Submit";
 import View from "./View";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
       <Router>
-        <header className="app__header">
-          Rate My Show
-        </header>
+        <Header />
         <div className={'app__sidebar'}>
           <ul>
             <li><Link to={'/'}>Home</Link></li>
