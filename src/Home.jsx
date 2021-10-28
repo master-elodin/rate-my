9ac1f9f0b227eb4e90760bab5ac85f6fd0738cef
Intifan <intifan@gmail.com>
2021-10-28 11:45:32 -0400

    made a .json list of departments, and fiddled with a table

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,9 +2,30 @@ import './App.css';
 
 const Home = () => {
   return (
+  <>
     <div>
-      this is the home page
-    </div>
+      <h1>Top Rated</h1>
+    </div><div>
+        <table>
+          <tr>
+            <th>The Worst</th>
+            <th>The Best</th>
+          </tr>
+          <tr>
+            <td>Name 1</td>
+            <td>Name 4</td>
+          </tr>
+          <tr>
+            <td>Name 2</td>
+            <td>Name 5</td>
+          </tr>
+          <tr>
+            <td>Name 3</td>
+            <td>Name 6</td>
+          </tr>
+        </table>
+      </div>
+  </>
   );
 }
 
--- src/Submit.jsx
+++ src/Submit.jsx
@@ -15,22 +15,22 @@ const Submit = () => {
       <div className={'submit__form'}>
         <div className={'submit__row'}>
           <h4>01. Name</h4>
-          <input type={'text'} placeholder={'Enter name'} autoFocus={true}
+          <input type={'text'} placeholder={'Enter the name of the person you\'re reviewing.'} autoFocus={true}
                  onChange={e => updateData('name', e.target.value)}/>
         </div>
         <div className={'submit__row'}>
           <h4>02. Show Name</h4>
-          <input type={'text'} placeholder={'Enter show name'} autoFocus={true}
+          <input type={'text'} placeholder={'What production did you work on with them?'} autoFocus={true}
                  onChange={e => updateData('showName', e.target.value)}/>
         </div>
         <div className={'submit__row'}>
-          <h4>03. Position</h4>
-          <input type={'text'} placeholder={'Enter position'} autoFocus={true}
+          <h4>03. Title/Position</h4>
+          <input type={'text'} placeholder={'Enter the person\'s title/position'} autoFocus={true}
                  onChange={e => updateData('position', e.target.value)}/>
         </div>
         <div className={'submit__row'}>
           <h4>04. Rate them</h4>
-          {[1, 2, 3].map(rating => (
+          {[1, 2, 3, 4, 5].map(rating => (
             <label key={rating}>
               <input type={'radio'} checked={data.rating === rating} onChange={() => updateData('rating', rating)}/>
               {rating}
@@ -47,14 +47,14 @@ const Submit = () => {
           ))}
         </div>
         <div className={'submit__row'}>
-          <h4>06. Have you been sexually harassed by them?</h4>
+          <h4>06. Have you been sexually harassed in any way by this person?</h4>
           {['No', 'Yes'].map(val => (
             <label key={val}>
               <input type={'radio'} checked={data.harassed === val} onChange={() => updateData('harassed', val)}/>
               {val}
             </label>
           ))}
-          <textarea placeholder={'Add more details'} onChange={e => updateData('harassedDetails', e.currentTarget.value)} />
+          <textarea placeholder={'Details you feel safe sharing.'} onChange={e => updateData('harassedDetails', e.currentTarget.value)} />
         </div>
         <div className={'submit__row'}>
           <h4>07. Are they safe?</h4>
@@ -69,13 +69,13 @@ const Submit = () => {
         <div className={'submit__row'}>
           <h4>08. Your department</h4>
           <select onChange={e => updateData('department', e.currentTarget.value)} value={data.department}>
-            {['Department 1','Department 2','Department 3','Department 4'].map(dep =>
+            {['Accounting','Art','Cast Member','Production Management','Producer','Special Effects'].map(dep =>
               <option key={dep} value={dep}>{dep}</option>)}
           </select>
         </div>
         <div className={'submit__row'}>
-          <h4>09. Additional thoughts</h4>
-          <textarea placeholder={'What\'s on your mind?'} onChange={e => updateData('freeformThoughts', e.currentTarget.value)} />
+          <h4>09. Detailed Review</h4>
+          <textarea placeholder={'What else do you think people need to know?'} onChange={e => updateData('freeformThoughts', e.currentTarget.value)} />
         </div>
         <div className={'submit__footer'}>
           <button type={'submit'} onClick={onSubmit}>Submit</button>
